e981dca5ac76c9da2a9579b70422420d30c4c796
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-03 16:14:38 +0530

    added some test code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from '@/lib/db'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const resp = await db({
+    text: 'SELECT 1;',
+    params: []
+  })
+  res.status(200).json({ name: 'John Doe', resp })
 }
