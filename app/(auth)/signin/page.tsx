180bdfbbbff441e2fa52d5ea739949b83f5ef45e
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-03 16:28:04 +0530

    fix

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,7 +4,7 @@ import "@/styles/global.css"
 const SigninPage = () => {
   return (
     <div className='pure-center' style={{height:"100%"}}>
-      ENV : {JSON.stringify(process.env)}
+      ENV : {JSON.stringify(process.env.DATABASE_URL)}
       <AuthForm mode='signin' />
     </div>
   )
