d9e0e18305cac48977e03c6bf22ed813065e1e31
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-03 16:38:47 +0530

    fix middleware

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,7 +4,6 @@ import "@/styles/global.css"
 const SigninPage = () => {
   return (
     <div className='pure-center' style={{height:"100%"}}>
-      db ENV : {JSON.stringify(process.env.DATABASE_URL)}
       <AuthForm mode='signin' />
     </div>
   )
