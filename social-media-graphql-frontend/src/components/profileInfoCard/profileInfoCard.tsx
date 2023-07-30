6689f196cab962261fee35c02e6c6758624ee4d9
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-07-30 18:41:53 +0530

    icon added to add friend and message button

--- social-media-graphql-frontend/src/components/profileInfoCard/profileInfoCard.tsx
+++ social-media-graphql-frontend/src/components/profileInfoCard/profileInfoCard.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Button } from "../ui/button";
 import AvatarLogo from "../avatar/AvatarLogo";
 import { ProfileInfoCardProps } from "../../models/component.model";
+import { BsPersonFillAdd, BsFillPersonCheckFill } from "react-icons/bs";
+import { AiFillMessage } from "react-icons/ai";
 
 const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({
   avatar,
   name,
   friendStatus,
   bio,
-  isOwnProfile=false,
+  isOwnProfile = false,
   displayType = "short",
 }) => {
   return (
@@ -22,19 +24,35 @@ const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({
           {/* Avatar */}
           <AvatarLogo image={avatar} text={name} />
           {/* Profile Name */}
-          <h2 className={`${displayType!=="short" ? "text-xl font-bold": "text-base"}`}>{name}</h2>
+          <h2
+            className={`${
+              displayType !== "short" ? "text-xl font-bold" : "text-base"
+            }`}
+          >
+            {name}
+          </h2>
         </div>
         {/* Friend Request or Friend Status */}
         {!isOwnProfile && (
-          <div className="flex flex-wrap gap-base justify-end items-center">
+          <div
+            className={`flex flex-wrap gap-base  ${
+              displayType === "short" && "justify-end"
+            } items-center`}
+          >
             <Button
               variant={friendStatus === "friend" ? "outline" : "default"}
-              className="mr-2"
+              className="mr-2 w-32"
             >
+              {friendStatus === "friend" ? (
+                <BsFillPersonCheckFill className="mr-2 h-4 w-4" />
+              ) : (
+                <BsPersonFillAdd className="mr-2 h-4 w-4" />
+              )}
               {friendStatus === "friend" ? "Friend" : "Add Friend"}
             </Button>
             {/* Message Button */}
-            <Button variant={friendStatus === "friend" ? "default" : "outline"}>
+            <Button className="w-32" variant={friendStatus === "friend" ? "default" : "outline"}>
+              <AiFillMessage className="mr-2 h-4 w-4" />
               Message
             </Button>
           </div>
