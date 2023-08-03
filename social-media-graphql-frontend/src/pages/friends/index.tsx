eacfae05e175898d0da388b57a2a8e49655368a3
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-08-03 11:32:12 +0530

    vercel config added

--- social-media-graphql-frontend/src/pages/friends/index.tsx
+++ social-media-graphql-frontend/src/pages/friends/index.tsx
@@ -1,8 +1,6 @@
 
-// import { fakeProfileInfoData } from "../../lib/fakeData";
-// import ManyUserCard from "../../components/ManyUserScrollAreaModel/ManyUserScrollAreaModel";
-
 const Friends = () => {
+
   return (
     // <ManyUserCard dataProp={fakeProfileInfoData} />
     <div>many users</div>
