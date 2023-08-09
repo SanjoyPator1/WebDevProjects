dfaf5df52c8330ed4c8935fde6f0a2a526ad5bd5
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-08-09 22:27:03 +0530

    notification bug fix : closure - with useRef

--- social-media-graphql-frontend/src/pages/notifications/Notifications.tsx
+++ social-media-graphql-frontend/src/pages/notifications/Notifications.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdNotifications } from "react-icons/md";
 import { useRecoilValue } from "recoil";
 import { userDataState } from "../../lib/recoil/atom";
@@ -9,24 +9,19 @@ import {
   NEW_NOTIFICATION,
 } from "../../graphql/queries/postQueries";
 import { NotificationModel } from "../../models/component.model";
-import { createSignal } from "../../lib/helperFunction";
 import { MARK_NOTIFICATIONS_SEEN } from "../../graphql/mutations/postMutations";
 import client from "../../graphql/apolloClient";
-
-//using signal to avoid closure
-const { getValue, setValue } = createSignal<boolean>(false);
 
 const NotificationsDropdown: React.FC = () => {
   const userData = useRecoilValue(userDataState);
   const [showNotifications, setShowNotifications] = useState(false);
   const [numberOfNewNotifications, setNumberOfNewNotifications] = useState(0);
 
-  const { data, loading, error } = useQuery(
-    GET_NOTIFICATIONS,
-    {
-      variables: { targetId: userData._id },
-    }
-  );
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const { data, loading, error } = useQuery(GET_NOTIFICATIONS, {
+    variables: { targetId: userData._id },
+  });
 
   const [markNotificationsAsSeen] = useMutation(MARK_NOTIFICATIONS_SEEN);
 
@@ -57,20 +52,16 @@ const NotificationsDropdown: React.FC = () => {
     onSubscriptionData: ({ subscriptionData }) => {
       if (subscriptionData.data) {
         const newNotification = subscriptionData.data.newNotification;
-  
+
         const prevDataCache = client.readQuery({
           query: GET_NOTIFICATIONS,
           variables: { targetId: userData._id },
         });
 
-        console.log({prevDataCache})
-        const prevData = prevDataCache.notifications
-        console.log({prevData})
-  
+        const prevData = prevDataCache.notifications;
+
         // Update the initial notifications with the new notification
         const updatedInitialNotifications = [newNotification, ...prevData];
-  
-        console.log({updatedInitialNotifications})
 
         // Update the cache with the new data
         client.writeQuery({
@@ -81,20 +72,25 @@ const NotificationsDropdown: React.FC = () => {
       }
     },
   });
-  
 
   if (error) {
     console.log({ error });
   }
 
   //handle on bell click
-  const handleBellClick = async () => {
-    const prevSignalValue = getValue();
+  const handleBellClick = async (isOutsideClick: boolean) => {
+    const prevShowNotificationValue = showNotifications;
     //toggle the open state of the notification list
-    setShowNotifications((prev) => !prev);
-    setValue(!prevSignalValue);
-    // if(prevNotificationState === true || prevSignalValue === true){
-    if (prevSignalValue === true) {
+    //when the notification list is already open and user clicks outside
+    if (isOutsideClick && prevShowNotificationValue) {
+      setShowNotifications((prev) => !prev);
+    }
+    //button click -> revert the state no matter what
+    if (!isOutsideClick) {
+      setShowNotifications((prev) => !prev);
+    }
+    //mark as seen true only when closing the notification list
+    if (prevShowNotificationValue === true) {
       //set the number of new notifications to 0 while closing
       setNumberOfNewNotifications(0);
 
@@ -103,14 +99,8 @@ const NotificationsDropdown: React.FC = () => {
         .filter((notification: NotificationModel) => !notification.seen)
         .map((notification: NotificationModel) => notification._id);
 
-      console.log("Unseen Notification IDs:");
-      console.log({ unseenNotificationIds });
-
       //if unseenNotification are present -> change seen to true : mutate
       if (unseenNotificationIds.length > 0) {
-        console.log(
-          "unseen notifications present so doing mutation to seen:true"
-        );
         // Call the mutation to mark notifications as seen
         await markNotificationsAsSeen({
           variables: {
@@ -165,55 +155,31 @@ const NotificationsDropdown: React.FC = () => {
       (notification: NotificationModel) => !notification.seen
     ).length;
 
-    console.log({ newNotificationsCount });
-
     // Set the number of new notifications
     setNumberOfNewNotifications(newNotificationsCount);
   }, [newNotificationData?.notifications, data?.notifications]);
 
-  // Close the notification list when clicking outside of it
   useEffect(() => {
-    const prevSignalValue = getValue();
-    const closeNotificationsOnOutsideClick = (event: MouseEvent) => {
-      if (!event.target) return;
-
-      const targetElement = event.target as HTMLElement;
-
-      const outsideClick = !targetElement.closest(".notification-dropdown");
-      const shouldClose = prevSignalValue !== true;
-
-      if (outsideClick && shouldClose) {
-        console.log(
-          "closing the notification dropdown without handleBellCLick"
-        );
-        setShowNotifications(false);
-        setValue(!prevSignalValue);
-      }
-
-      if (outsideClick && !shouldClose) {
-        console.log("calling handle bell click")
-        handleBellClick();
+    const handler = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        handleBellClick(true);
       }
     };
 
-    window.addEventListener("click", closeNotificationsOnOutsideClick);
+    document.addEventListener("mousedown", handler);
 
     return () => {
-      window.removeEventListener("click", closeNotificationsOnOutsideClick);
+      document.removeEventListener("mousedown", handler);
     };
-  }, []);
-
-  console.log({ notificationData: data?.notifications });
+  }, [handleBellClick, menuRef]);
 
   return (
-    <div className="px-4 py-2 relative notification-dropdown">
+    <div className="px-4 py-2 relative notification-dropdown" ref={menuRef}>
       <div
-        onClick={() => handleBellClick()}
+        onClick={() => handleBellClick(false)}
         className="relative cursor-pointer"
       >
         <MdNotifications className="h-6 w-5 md:h-6 md:w-6" />
-        {/* {showNotifications.toString()} */}
-        {/* {prevSignalValue.toString()} */}
         {numberOfNewNotifications > 0 && (
           <div className="bg-destructive rounded-full h-4 w-4 text-xs text-white text-center absolute -top-1 -right-1">
             {numberOfNewNotifications}
@@ -223,11 +189,7 @@ const NotificationsDropdown: React.FC = () => {
       {showNotifications && (
         <div className="absolute right-0 bottom-100 border rounded-md bg-primary-foreground mt-3 md:mt-6">
           {!loading && (
-            <NotificationSheet
-              notificationData={
-                data?.notifications || []
-              }
-            />
+            <NotificationSheet notificationData={data?.notifications || []} />
           )}
         </div>
       )}
