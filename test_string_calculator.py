da93d794271d3897b42b7bc866de053acb5a840e
SanjoyPator1 <sanjoypator1@gmail.com>
2025-04-15 11:26:59 +0530

    string_calculator_tdd: Test: Add failing test for newline as delimiter

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -17,5 +17,8 @@ class TestStringCalculator(unittest.TestCase):
     def test_multiple_numbers_returns_sum(self):
         self.assertEqual(15, self.calculator.add("1,2,3,4,5"))
 
+    def test_newline_as_delimiter(self):
+        self.assertEqual(6, self.calculator.add("1\n2,3"))
+
 if __name__ == "__main__":
     unittest.main()
